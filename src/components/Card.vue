e9cf90258ac1d450d4e04cf005f18f5101c23c92
acrylic-style <me@acrylicstyle.xyz>
2021-09-30 11:59:38 +0900

    Implement changePassword function

---
+++ src/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="not-flexing-card z-depth-5" style="align-items: center;">
+    <slot />
+  </section>
+</template>
+
+<style>
+.not-flexing-card>div.col {
+  float: inherit;
+}
+</style>
+
+<style scoped>
+.not-flexing-card {
+  word-wrap: break-word;
+  position: relative;
+  flex-direction: column;
+  background-color: #22282a;
+  background-clip: border-box;
+  border: 1px solid rgba(0, 0, 0, 0.125);
+  border-radius: .25rem;
+  margin: .5rem .5rem 1rem .5rem;
+  padding: 2rem;
+}
+</style>
--- src/pages/me/App.vue
+++ src/pages/me/App.vue
@@ -4,14 +4,14 @@
   <Container v-if="me">
     <div class="row">
       <div class="col s12">
-        <div class="not-flexing-card z-depth-5" style="align-items: center;">
+        <Card>
+          <h2>ユーザー名変更</h2>
           <InputTextField
               label="ユーザー名"
               :min-length="4"
               :max-length="32"
               :default-value="me.username"
               id="me_username"
-              div-class="s11"
               white-text
               active-label
               ref="me_username"
@@ -19,7 +19,40 @@
               :disabled="disableForm"
           />
           <Button text="変更" :disabled="disableForm" @click="changeName()" />
-        </div>
+        </Card>
+        <Card>
+          <h2>パスワード変更</h2>
+          <InputTextField
+              label="現在のパスワード"
+              :min-length="7"
+              id="me_current_password"
+              white-text
+              ref="me_current_password"
+              type="password"
+              :disabled="disableForm"
+          />
+          <InputTextField
+              label="新しいパスワード"
+              :min-length="7"
+              id="me_new_password"
+              white-text
+              ref="me_new_password"
+              type="password"
+              :disabled="disableForm"
+          />
+          <InputTextField
+              label="新しいパスワード (確認)"
+              :min-length="7"
+              id="me_new_password_confirm"
+              white-text
+              ref="me_new_password_confirm"
+              type="password"
+              :disabled="disableForm"
+          />
+          <div class="col s12">
+            <Button text="変更" :disabled="disableForm" @click="changePassword()" />
+          </div>
+        </Card>
       </div>
     </div>
   </Container>
@@ -33,11 +66,13 @@ import Preloader from '@/components/Preloader.vue'
 import InputTextField from '@/components/InputTextField.vue'
 import Button from '@/components/Button.vue'
 import {isValidName, toast} from '@/util/util'
+import Card from '@/components/Card.vue'
 
 const me = ref(null)
 
 export default {
   components: {
+    Card,
     Button,
     Preloader,
     Container,
@@ -56,6 +91,7 @@ export default {
       if (!isValidName(username)) {
         return toast('このユーザー名は使用できません。')
       }
+      toast('パスワードを変更中...')
       this.disableForm = true
       fetch(`${process.env.VUE_APP_API_URL}/i_users/changename`, {
         method: 'POST',
@@ -90,6 +126,45 @@ export default {
         this.disableForm = false
       })
     },
+    changePassword() {
+      if (this.disableForm) return
+      const currentPassword = this.$refs.me_current_password.value
+      const newPassword = this.$refs.me_new_password.value
+      if (currentPassword < 7) return
+      if (newPassword !== this.$refs.me_new_password_confirm.value) {
+        return toast('パスワード (確認)が一致しません。')
+      }
+      if (newPassword.length < 7) {
+        return toast('パスワードは7文字以上にする必要があります。')
+      }
+      this.disableForm = true
+      fetch(`${process.env.VUE_APP_API_URL}/i_users/changepassword`, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'X-SpicyAzisaBan-Session': localStorage.getItem("spicyazisaban_session"),
+        },
+        body: JSON.stringify({
+          user_id: this.$refs.navbar.user.id,
+          currentPassword,
+          newPassword,
+        }),
+      }).then(res => res.json()).then(res => {
+        const err = res.error
+        if (err) {
+          if (err === 'incorrect_password') {
+            toast('パスワードが間違っています。')
+          } else {
+            toast(`不明なエラーが発生しました。 (${err})`)
+          }
+          return
+        }
+        toast('パスワードを変更しました。')
+      }).finally(() => {
+        this.disableForm = false
+      })
+    },
   },
   setup() {
     fetch(`${process.env.VUE_APP_API_URL}/i_users/me`, {
@@ -119,19 +194,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #fff;
-}
-</style>
-
-<style scoped>
-.not-flexing-card {
-  word-wrap: break-word;
-  position: relative;
-  flex-direction: column;
-  background-color: #22282a;
-  background-clip: border-box;
-  border: 1px solid rgba(0, 0, 0, 0.125);
-  border-radius: .25rem;
-  margin: .5rem .5rem 1rem .5rem;
-  padding: 2rem;
 }
 </style>
